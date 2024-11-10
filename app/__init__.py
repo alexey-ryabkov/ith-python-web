10b00af460ca454675b5318d5472ad8e6900fc8d
alexey-ryabkov <xelaris@ya.ru>
2024-11-10 16:53:03 +0300

    fix: rework db setup 2 init data once at start app

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from .config import APP_CONFIG
 from .session import is_user_authorized
 from .utils import get_cur_user_data
+from .db.manager import DatabaseManager
 
 
 app = Flask(
@@ -10,10 +11,19 @@ app = Flask(
     static_folder=os.path.join(os.path.dirname(__file__), "..", "static"),
 )
 app.secret_key = os.urandom(24)
+just_started = True
+
+
+@app.before_request
+def init_db_data_on_start():
+    global just_started
+    if just_started:
+        DatabaseManager().init_data()
+        just_started = False
 
 
 @app.context_processor
-def inject_utils():
+def define_context():
     return dict(
         is_user_authorized=is_user_authorized(),
         user_data=get_cur_user_data(),
--- app/db/manager.py
+++ app/db/manager.py
@@ -12,26 +12,13 @@ class DatabaseManager:
     def __init__(self):
         try:
             self.conn = mysql.connector.connect(**APP_CONFIG["db"])
-            self.cursor = self.conn.cursor(dictionary=True)
-            self.prepare()
         except mysql.connector.Error as e:
             if e.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-                # `ll try to create db if it appears to be unknown
-                db_config_wo_database = {
-                    k: v for k, v in APP_CONFIG["db"].items() if k != "database"
-                }
-                self.conn = mysql.connector.connect(**db_config_wo_database)
-
-                cursor = self.conn.cursor(dictionary=True)
-                cursor.execute(f"CREATE DATABASE {APP_CONFIG['db']['database']}")
-                self.conn.commit()
-                cursor.execute(f"USE {APP_CONFIG['db']['database']}")
-                cursor.close()
-
-                self.cursor = self.conn.cursor(dictionary=True)
-                self.prepare()
+                self._create_db()
             else:
                 raise e
+        finally:
+            self.cursor = self.conn.cursor(dictionary=True)
 
     def __enter__(self):
         return self.cursor
@@ -41,13 +28,24 @@ class DatabaseManager:
         self.cursor.close()
         self.conn.close()
 
-    def prepare(self):
-        # print("prepare!!!")
+    def init_data(self):
         self._create_tables()
         self.cursor.execute("SELECT COUNT(*) as cnt FROM Event")
+        # if no any events, load init data
         if self.cursor.fetchone()["cnt"] == 0:
             self._load_init_data()
         self.conn.commit()
+
+    def _create_db(self):
+        db_config_wo_database = {
+            k: v for k, v in APP_CONFIG["db"].items() if k != "database"
+        }
+        self.conn = mysql.connector.connect(**db_config_wo_database)
+        cursor = self.conn.cursor(dictionary=True)
+        cursor.execute(f"CREATE DATABASE {APP_CONFIG['db']['database']}")
+        self.conn.commit()
+        cursor.execute(f"USE {APP_CONFIG['db']['database']}")
+        cursor.close()
 
     def _create_tables(self):
         execute_sql_file(self.cursor, _get_path("create_tables.sql"))
